9ebbf6b421b3e9df8af163ab231e9fb415ce6cb2
Fortyseven <fortyseven@gmail.com>
2015-08-27 15:19:59 -0400

    Added gradual difficulty increase for rock drops

--- Assets/01-Dodger/scripts/Main.cs
+++ Assets/01-Dodger/scripts/Main.cs
@@ -5,21 +5,25 @@ using Random = UnityEngine.Random;
 
 public class Main : MonoBehaviour
 {
-    private const int MAX_ROCKS = 1;
+    private const int MAX_ROCKS = 40;
     private const float WALL_X = 3.0f;
     private const float ROCK_SPAWN_Y = 5.5f;
+    private const float MAX_CRAZY_TIME = 100.0f;
 
     public GameObject RockPrefab;
 
     public Text UI_Clock;
     public Text UI_ClockBest;
 
-    private float _game_time = 0.0f;
+    private float _game_time;
     private float _best_game_time = 0.0f;
 
     public float GameTime { get { return _game_time; } }
 
     private GameObject[] _rocks = null;
+
+    private int _num_active_rocks = 0;
+
 
     /****************************************/
     void Start()
@@ -31,6 +35,8 @@ public class Main : MonoBehaviour
         for ( int i = 0; i < MAX_ROCKS; i++ ) {
             _rocks[ i ] = (GameObject)Instantiate( RockPrefab, new Vector3( Random.Range( -WALL_X, WALL_X ), ROCK_SPAWN_Y, 0 ), Quaternion.identity );
             _rocks[ i ].GetComponent<Rock>().SetOwner( this );
+            // Rocks start off disabled; will be enabled slowly in wave manager
+            _rocks[ i ].SetActive( false );
         }
     }
 
@@ -38,6 +44,18 @@ public class Main : MonoBehaviour
     void Update()
     {
         updateScoreBoard();
+
+        // Adjust difficulty by gradually adding more rocks until we hit MAX_CRAZY_TIME where 
+        // it's just all the rocks forever and ever.
+        _num_active_rocks = Mathf.Clamp( Mathf.RoundToInt( MAX_ROCKS * ( _game_time / MAX_CRAZY_TIME ) ), 0, MAX_ROCKS );
+
+        // Update which rocks are active
+        for ( int i = 0; i < _num_active_rocks; i++ ) {
+            if ( _rocks[ i ].activeInHierarchy )
+                continue;
+
+            _rocks[ i ].SetActive( true );
+        }
     }
 
     /****************************************/
@@ -50,6 +68,5 @@ public class Main : MonoBehaviour
             _best_game_time = _game_time;
             UI_ClockBest.text = time_formatted;
         }
-
     }
 }
